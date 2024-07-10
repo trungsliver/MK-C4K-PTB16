033748df0a82e4a359a2098d38c9430cdf296613
trungsliver <buiductrung.cva@gmail.com>
2024-07-10 20:49:33 +0700

    Lesson 5

---
+++ Lesson5.py
@@ -0,0 +1,20 @@
+    # Trắc nghiệm
+# 1. Mình đã học bảo nhiêu kiểu dữ liệu, đó là những kiểu dữ liệu nào?
+# 2. Câu lệnh nhập dữ liệu từ bàn phím là gì?
+# 3. Câu lệnh in dữ liệu ra màn hình là gì?
+# 4. Mình đã học những phép toán nào trong lập trình?
+# 5. Biến số là gì, nêu tác dụng của biến số trong lập trình?
+# 6. Có bao nhiêu cấu trúc câu điều kiện?
+
+    # Tự luận
+# Bài 1: Nhập vào từ bàn phím các thông tin các nhân: Họ tên, Tuổi, Trường học, giới tính, điểm trung bình.
+    # In ra màn hình các thông tin vừa nhập
+
+# Bài 2: Nhập vào chiều dài và chiều rộng hình chữ nhật
+    # In ra màn hình chu vi và diện tích hình chữ nhật đó.
+
+# Bài 3: Nhập số nguyên n từ bàn phím.
+    # Kiểm tra xem n có phải là số chẵn hay không.
+
+# Bài 4: Nhập điểm số từ bàn phím. Hãy in ra mà hình xếp hạng học lực, biết rằng:
+    # 8-10: Giỏi, 6.5-8: Khá, 5-6.5: Trung Bình, 0-5: Yếu
---
+++ PTB16_TungLam_CP1.py
@@ -0,0 +1,60 @@
+# bài 1
+# name = str(input('hãy nhập tên của bạn :'))
+# print('họ tên:',name)
+# age =int(input('hãy nhập tuổi của bạn :'))
+# print('tuổi:',age)
+# school = str(input('hãy nhập tên trường của bạn:'))
+# print('trường:',school)
+# gioitinh =str(input('hãy nhập giới tính của bạn:'))
+# print('giới tính:',gioitinh)
+# point = float(input('hãy nhập điểm của bạn: '))
+# print('điểm trung bình:',point)
+# # bài2
+# a = float(input('hãy nhập chiều dài hcn:'))
+# b = float(input('hãy nhập chiều rông hcn:'))
+# chuvi = (a+b)*2
+# dientich = a*b
+# print('chu vi hcn là:',chuvi)
+# print('diện tích hcn là :',dientich)
+# # bài3
+n = int(input('hãy nhập số nguyên: '))
+print('số nguyên:',n)
+if n % 2 == 0:
+    print(n, 'là số chẵn')
+else:
+    print(n, 'là số lẻ')
+# bài4
+# point = float(input('Hãy nhập điểm của bạn: '))
+
+# if 0 <= point <= 10:
+#     if 8 <= point <= 10:
+#         print('Xếp loại: Giỏi')
+#     elif 6.5 <= point < 8:
+#         print('Xếp loại: Khá')
+#     elif 5 <= point < 6.5:
+#         print('Xếp loại: Trung Bình')
+#     else:
+#         print('Xếp loại: Yếu')
+# else:
+#     print('Bạn đã nhập sai')
+# câu 1:
+    # string:chuỗi kí tự - xâu kí tự
+    # int:số nguyên (ko có  thập phân )
+    # float:số thực ( có thập phân)
+    # bool:boolean/true/flase:đúng/sai 
+# câu 2
+    # input
+# câu 3
+    # print
+# câu 4
+    # các phép toán :+-*/
+    # chia lấy dư :(%)
+    # chia lấy nguyên (//)
+    # luỹ thừa (**)
+# câu 5
+#  #  chỉ bao gồm chữ ,số 0-9,ký tự gạch dưới 
+ # không được bắt đầu bắng số 
+ # phân biệt chữ hoa và chữ thường 
+ # không được trùng từ khoá 
+# câu 6
+    # có 3 cấu trúc:(dạng đa nhánh dạng đủ dạng thiếu )
