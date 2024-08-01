cb9c3530fdffbcc3d554bf5ad1df9d2976947a55
trungsliver <buiductrung.cva@gmail.com>
2024-08-01 17:17:13 +0700

    lesson 7

---
+++ Leson7.py
@@ -0,0 +1,23 @@
+# Vòng lặp while - Vòng lặp vô hạn
+# for i in range(1,6):
+#     print(i)
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+
+# Đề bài: DÙng vòng lặp while, in ra màn hình số nguyên từ 5 đến 10
+# i = 5
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+# Đề bài 1: Nhập số nguyên n trong khoảng [0,100]
+# nếu nhập sai (n<0 hoặc n>100) thì yêu cầu nhập lại
+# n = int(input('Nhập n trong khoảng [0,100]: '))
+# while n<0 or n>100:
+#     print('Bạn đã nhập sai, vui lòng nhập lại!')
+#     n = int(input('\nNhập n trong khoảng [0,100]: '))
+# print('Bạn đã nhập đúng!')
+
